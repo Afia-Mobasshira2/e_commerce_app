ec0a282880092ff0cd36cb5fe5cfb3a82fdc6ba8
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-01-02 17:22:42 +0600

    new update

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+class TImages {
+
+//App logos
+  static const String darkAppLogo = "";
+  static const String lightAppLogo = "";
+
+  //Social logo
+  static const String google = "";
+  static const String facebook = "";
+
+}
