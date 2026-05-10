3b0d2fd2ba5fb93275613bc7c231a9e31809c508
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-10 17:31:35 +0600

    new update

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -4,6 +4,7 @@ import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/constants/text_strings.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -31,7 +32,22 @@ class LoginScreen extends StatelessWidget {
                     Text(TTexts.loginSubTitle, style: Theme.of(context).textTheme.bodyMedium,)
 
                 ],
-              )
+              ),
+
+              Form(
+                child: Column(
+                  children: [
+                    //Email
+                    TextFormField(
+                      decoration: const InputDecoration(prefixIcon: Icon(Iconsax.direct_right),labelText: TTexts.email),
+                    ),
+                    //Password
+                    TextFormField(
+                      decoration: const InputDecoration(prefixIcon: Icon(Iconsax.password_check),labelText: TTexts.password),
+                    ),
+                    const SizedBox(height: TSizes.spaceBtwinputFields / 2,)
+                  ],
+                ))
             ],),
           ),
       ),
