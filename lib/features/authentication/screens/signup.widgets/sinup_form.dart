efe9dad7481bc763439293fa79d013367faaa666
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-12 17:11:05 +0600

    new update

--- lib/features/authentication/screens/signup.widgets/sinup_form.dart
+++ lib/features/authentication/screens/signup.widgets/sinup_form.dart
@@ -1,49 +1,8 @@
-import 'package:e_commerce_app/common/widgets/login_signup/form_divider.dart';
-import 'package:e_commerce_app/common/widgets/login_signup/social_button.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/constants/text_strings.dart';
-import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_utils/src/extensions/export.dart';
 import 'package:iconsax/iconsax.dart';
-
-class SignupScreen extends StatelessWidget {
-  const SignupScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(TSizes.defaultSpace),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //Title
-              Text(TTexts.signupTitle,style: Theme.of(context).textTheme.headlineMedium,),
-              const SizedBox(height: TSizes.spaceBtwSections,),
-
-              //Form
-              TSignupForm(dark: dark),
-              const SizedBox(height: TSizes.spaceBtwSections,),
-              
-
-              //Divider
-             TFormDivider(dividerText: TTexts.orSignUpWith.capitalize!),
-              const SizedBox(height: TSizes.spaceBtwSections,),
-
-
-              //Social Button
-              const TSocialButton()
-            ],
-          ),),
-      ),
-    );
-  }
-}
 
 class TSignupForm extends StatelessWidget {
   const TSignupForm({
