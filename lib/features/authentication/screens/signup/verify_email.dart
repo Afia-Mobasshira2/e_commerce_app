6edd179beba1827a40911357f005f0b9a88bab3a
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-13 22:31:31 +0600

    new update

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_app/features/authentication/screens/login/login.dart';
 import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/constants/text_strings.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -30,10 +31,19 @@ class VerifyEmailScreen extends StatelessWidget {
                     ),
                    const SizedBox(height: TSizes.spaceBtwSections,),
 
-
                   //Title & Subtitle
+                    Text(TTexts.confirmEmail,style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center,),
+                   const SizedBox(height: TSizes.spaceBtwItems,),
+                    Text(TTexts.confirmEmail,style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center,),
+                   const SizedBox(height: TSizes.spaceBtwItems,),
+                    Text(TTexts.confirmEmail,style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center,),
+                   const SizedBox(height: TSizes.spaceBtwSections,),
 
                   //buttons
+                  SizedBox(width: double.infinity,child: ElevatedButton(onPressed: (){}, child: const Text(TTexts.tContinue)),),
+                   const SizedBox(height: TSizes.spaceBtwItems,),
+
+                  SizedBox(width: double.infinity,child: ElevatedButton(onPressed: (){}, child: const Text(TTexts.resendEmail)),)
                 ],
               ),
               ),
