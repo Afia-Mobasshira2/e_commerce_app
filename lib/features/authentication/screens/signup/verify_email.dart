79d9d0f17949e6f62f9d7f82f9cfc57bfb9be81d
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-13 20:41:57 +0600

    new update

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_app/features/authentication/screens/login/login.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -16,13 +18,18 @@ class VerifyEmailScreen extends StatelessWidget {
           IconButton(onPressed: () => Get.offAll(() => LoginScreen()), icon: const Icon(CupertinoIcons.clear),)
         ],
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
             //Padding to give defaul Equal space on all sides in all screen
             child: Padding(
               padding: EdgeInsets.all(TSizes.defaultSpace),
               child: Column(
                 children: [
                   //Image
+                    Image(image: AssetImage(TImages.verify),
+                    width: THelperFunctions.screenWidth() * 0.6,
+                    ),
+                   const SizedBox(height: TSizes.spaceBtwSections,),
+
 
                   //Title & Subtitle
 
