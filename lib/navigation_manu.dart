92b6654c844b4868c6051b6f06b46b84f8d8a350
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-15 22:08:02 +0600

    new update

---
+++ lib/navigation_manu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavigationManu extends StatelessWidget {
+  const NavigationManu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
