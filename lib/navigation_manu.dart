3ad1660290ace37da733d22d16f613a36fca3c16
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-16 18:14:13 +0600

    new update

--- lib/navigation_manu.dart
+++ lib/navigation_manu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 
 class NavigationManu extends StatelessWidget {
@@ -6,19 +8,30 @@ class NavigationManu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        height: 80,
-        elevation: 0,
-        selectedIndex: 0,
-        destinations: const [
-        NavigationDestination(icon:Icon(Iconsax.home,color: Colors.green),label: 'Home',),
-        NavigationDestination(icon:Icon(Iconsax.shop,color: Colors.blue),label: 'Store'),
-        NavigationDestination(icon:Icon(Iconsax.heart,color: Colors.deepPurple,),label: 'Wishlist'),
-        NavigationDestination(icon:Icon(Iconsax.user,color: Colors.orange,),label: 'Profile'),
-        ]
-         ),
+      bottomNavigationBar: Obx(//Obx observer
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          
+          destinations: const [
+          NavigationDestination(icon:Icon(Iconsax.home,color: Colors.green),label: 'Home',),
+          NavigationDestination(icon:Icon(Iconsax.shop,color: Colors.blue),label: 'Store'),
+          NavigationDestination(icon:Icon(Iconsax.heart,color: Colors.deepPurple,),label: 'Wishlist'),
+          NavigationDestination(icon:Icon(Iconsax.user,color: Colors.orange,),label: 'Profile'),
+          ]
+           ),
+      ),
          body: Container(),
     );
   }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs; ///obs being observed
+
+  final screens = [Container(color: Colors.green),Container(color: Colors.blue),Container(color: Colors.deepPurple),Container(color: Colors.orange,)];
 }
