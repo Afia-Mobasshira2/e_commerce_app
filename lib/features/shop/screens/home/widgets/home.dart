d9edf2b1754283034a77d9322595e6fb777e57b7
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-16 21:15:06 +0600

    new update

--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: TColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
