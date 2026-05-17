145f11e044f1adcfb78c397a02c6bef94aa0bcfc
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-17 11:35:30 +0600

    new update

--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -30,8 +30,21 @@ class HomeScreen extends StatelessWidget {
 
 class TCircularContainer extends StatelessWidget {
   const TCircularContainer({
-    super.key,
+    super.key, 
+    this.width = 400, 
+    this.height = 400,  
+    this.radius = 400, 
+    this.padding = 0, 
+    this.child, 
+    this.backgroundColor = TColors.white,
   });
+
+    final double? width;
+    final double? height;
+    final double radius;
+    final double padding;
+    final Widget? child;
+    final Color backgroundColor;
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +56,7 @@ class TCircularContainer extends StatelessWidget {
         borderRadius: BorderRadius.circular(400),
         color: TColors.textWhite.withOpacity(0.1),
       ),
+      child: child,
     );
   }
 }
