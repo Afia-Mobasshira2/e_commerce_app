dee00357703df131d1ac75b21957edce7753827e
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-17 20:06:12 +0600

    new update

--- lib/common/widgets/custom_shapes/curved_edges/curved_edges.dart
+++ lib/common/widgets/custom_shapes/curved_edges/curved_edges.dart
@@ -14,6 +14,10 @@ class TCustomCurvedEdges extends CustomClipper<Path>{
     final secondLastCurve = Offset(size.width-30, size.height - 20);
     path.quadraticBezierTo(secondFirstCurve.dx, secondFirstCurve.dy, secondLastCurve.dx, secondLastCurve.dy);
 
+    final thirdFirstCurve = Offset(size.width, size.height - 20);
+    final thirdLastCurve = Offset(size.width, size.height);
+    path.quadraticBezierTo(thirdFirstCurve.dx, thirdFirstCurve.dy, thirdLastCurve.dx, thirdLastCurve.dy);
+
     path.lineTo(size.width, 0);
     path.close();
     return path; 
