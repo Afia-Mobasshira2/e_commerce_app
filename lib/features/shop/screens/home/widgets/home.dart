a984a1b18789aa2d5a2383fb8306db502dc92df8
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-17 22:55:23 +0600

    new update

--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -12,25 +12,36 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ClipPath(
-              clipper: TCustomCurvedEdges(),
-              child: Container(
-                color: TColors.primary,
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(top: -150,right: -250,child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1),)),
-                      Positioned(top: 100, right: -300,child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1),),)
-                  
-                      
-                    ],
-                  ),
-                ),
-              ),
-            )
+            TCustomCurveEdges()
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class TCustomCurveEdges extends StatelessWidget {
+  const TCustomCurveEdges({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TCustomCurvedEdges(),
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(top: -150,right: -250,child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1),)),
+              Positioned(top: 100, right: -300,child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1),),)
+          
+              
+            ],
+          ),
         ),
       ),
     );
