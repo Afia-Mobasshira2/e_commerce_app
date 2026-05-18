a297864262caefbf83bd1582e283d39f35b1458f
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-18 22:32:53 +0600

    new update

--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -4,6 +4,7 @@ import 'package:e_commerce_app/common/widgets/custom_shapes/container/primary_he
 import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:e_commerce_app/common/widgets/products.cart/cart_menu_icon.dart';
+import 'package:e_commerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
@@ -20,18 +21,7 @@ class HomeScreen extends StatelessWidget {
           children: [
             TPrimaryHeaderContainer(child: Column(
               children: [
-                TAppBar(
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(TTexts.homeAppBarTitle,style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey),),
-                      Text(TTexts.homeAppBarSubTitle,style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white),),
-                    ],
-                  ),
-                  actions: [
-                    TCartCounterIcon(onPressed: (){}, iconColor: TColors.white,),
-              ],
-                ),
+                THomeAppBar(),
               ],
             ),),
           ],
@@ -40,4 +30,6 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
 
