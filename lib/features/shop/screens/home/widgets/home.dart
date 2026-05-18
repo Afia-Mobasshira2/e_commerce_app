0d44aeff75af7a940f5bba2875f78a2ab6a26696
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-18 20:52:59 +0600

    new update

--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -28,15 +28,44 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                   actions: [
-                    IconButton(onPressed: (){}, icon: const Icon(Iconsax.shopping_bag,color: TColors.white,)),
-                  ],
-                )
+                    TCartCounterIcon(),
+              ],
+                ),
               ],
             ),),
           ],
         ),
       ),
     );
+  }
+}
+
+class TCartCounterIcon extends StatelessWidget {
+  const TCartCounterIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+    IconButton(onPressed: (){}, icon: const Icon(Iconsax.shopping_bag,color: TColors.white,)),
+    Positioned(
+      right: 0,
+    child: Container(
+      width: 18,
+      height: 18,
+      decoration: BoxDecoration(
+        color: TColors.black,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Center(
+        child: Text('2',style: Theme.of(context).textTheme.labelLarge!.apply(color: TColors.white,fontSizeFactor: 0.8),),///fontSizeFactor: 0.8 na dila 2 likha boro dakhai
+      ),
+    ),
+    ),
+                      ],
+                    );
   }
 }
 
