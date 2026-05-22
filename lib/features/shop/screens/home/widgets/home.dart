78de78ee2f55928626edd1a3643808d05411134e
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-22 13:03:44 +0600

    new update

--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -31,12 +31,50 @@ class HomeScreen extends StatelessWidget {
                 const SizedBox(height: TSizes.defaultSpace,),
 
                 //Search bar
-                TSearchContainer(text: 'Search in Store',)
+                TSearchContainer(text: 'Search in Store',),
+                const SizedBox(height: TSizes.spaceBtwSections,),
+
+                //Categories
+                Padding(
+                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      TSectionHeading(title: 'Popular Categories',showActionButton: false,),
+                    ],
+                  ),
+                  )
+
               ],
             ),),
           ],
         ),
       ),
+    );
+  }
+}
+
+class TSectionHeading extends StatelessWidget {
+  const TSectionHeading({
+    super.key, 
+    this.textColor, 
+    this.showActionButton = true, 
+    required this.title, 
+    this.buttonTitle = 'View All', 
+    this.onPressed,
+  });
+
+  final Color? textColor;
+  final bool showActionButton;
+  final String title,buttonTitle;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(title, style: Theme.of(context).textTheme.headlineSmall?.apply(color: textColor),maxLines: 1,overflow: TextOverflow.ellipsis,),
+        if(showActionButton) TextButton(onPressed: onPressed, child:  Text(buttonTitle)),
+      ],
     );
   }
 }
