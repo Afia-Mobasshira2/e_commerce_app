1ff058fa54c8d7b6815d931dbe7ae8978c70da26
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-24 22:25:01 +0600

    new update

--- lib/common/widgets/custom_shapes/container/search_container.dart
+++ lib/common/widgets/custom_shapes/container/search_container.dart
@@ -7,33 +7,42 @@ import 'package:iconsax/iconsax.dart';
 
 class TSearchContainer extends StatelessWidget {
   const TSearchContainer({
-    super.key, required this.text, this.icon, this.showBackground = true, this.showBorder =true,
+    super.key, 
+    required this.text, 
+    this.icon, 
+    this.showBackground = true, 
+    this.showBorder =true, 
+    this.onTap,
   });
 
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-      child: Container(
-        width: TDeviceUtils.getScreenWidth(context),
-        padding: EdgeInsets.all(TSizes.md),
-        decoration: BoxDecoration(
-          color: showBackground ? dark ? TColors.dark : TColors.light : Colors.transparent,
-          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-          border: showBorder ? Border.all(color: TColors.grey) : null,
-        ),
-        child: Row(
-          children: [
-            Icon(Iconsax.search_normal, color: TColors.darkerGrey,),
-            const SizedBox(width: TSizes.spaceBtwItems,),
-            Text('Search in Store',style: Theme.of(context).textTheme.bodySmall),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        child: Container(
+          width: TDeviceUtils.getScreenWidth(context),
+          padding: EdgeInsets.all(TSizes.md),
+          decoration: BoxDecoration(
+            color: showBackground ? dark ? TColors.dark : TColors.light : Colors.transparent,
+            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: TColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(Iconsax.search_normal, color: TColors.darkerGrey,),
+              const SizedBox(width: TSizes.spaceBtwItems,),
+              Text('Search in Store',style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
         ),
       ),
     );
