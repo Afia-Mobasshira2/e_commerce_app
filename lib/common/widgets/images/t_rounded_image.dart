450eb058345cc6a2ab15a82426496d814e2f39a4
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-29 18:59:01 +0600

    new update

--- lib/common/widgets/images/t_rounded_image.dart
+++ lib/common/widgets/images/t_rounded_image.dart
@@ -1,1 +1,57 @@
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key, 
+    this.width, 
+    this.height, 
+    required this.imageUrl, 
+    this.applyImageRadius = true, 
+    this.border, 
+    this.backgroundColor = TColors.light, 
+    this.fit = BoxFit.cover, 
+    this.padding, 
+    this.isNetworkImage = false, 
+    this.onPressed, 
+    this.borderRadius = TSizes.md,
+  });
+
+  final double? width,height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetworkImage;
+  final VoidCallback? onPressed; 
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        //width : width,
+        //height:height,
+        padding: padding,
+      decoration: BoxDecoration(border: border,color: backgroundColor,borderRadius: BorderRadius.circular(borderRadius)),
+        //color: Colors.red,
+      width: double.infinity,
+      height: 150, // This strictly forces the layout box height
+         
+        child: ClipRRect(
+          borderRadius: applyImageRadius ? BorderRadius.circular(borderRadius) :BorderRadius.zero,
+          child:  Image(fit:fit, image: isNetworkImage ? NetworkImage(imageUrl) : AssetImage(imageUrl) as ImageProvider,
+           // fit: BoxFit.contain,     /// 3. This forces the image to crop/scale into the box
+           //AssetImage(TImages.promoBanner2)
+            ),
+          ),
+      
+        ),
+    );
+  }
+}
+
--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -5,6 +5,7 @@ import 'package:e_commerce_app/common/widgets/custom_shapes/container/search_con
 import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:e_commerce_app/common/widgets/image_text_widgets/vertical_image_text.dart';
+import 'package:e_commerce_app/common/widgets/images/t_rounded_image.dart';
 import 'package:e_commerce_app/common/widgets/products.cart/cart_menu_icon.dart';
 import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widgets/home_appbar.dart';
@@ -67,59 +68,6 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class TRoundedImage extends StatelessWidget {
-  const TRoundedImage({
-    super.key, 
-    this.width, 
-    this.height, 
-    required this.imageUrl, 
-    this.applyImageRadius = true, 
-    this.border, 
-    this.backgroundColor = TColors.light, 
-    this.fit = BoxFit.cover, 
-    this.padding, 
-    this.isNetworkImage = false, 
-    this.onPressed, 
-    this.borderRadius = TSizes.md,
-  });
-
-  final double? width,height;
-  final String imageUrl;
-  final bool applyImageRadius;
-  final BoxBorder? border;
-  final Color backgroundColor;
-  final BoxFit? fit;
-  final EdgeInsetsGeometry? padding;
-  final bool isNetworkImage;
-  final VoidCallback? onPressed; 
-  final double borderRadius;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        //width : width,
-        //height:height,
-        padding: padding,
-      decoration: BoxDecoration(border: border,color: backgroundColor,borderRadius: BorderRadius.circular(borderRadius)),
-        //color: Colors.red,
-      width: double.infinity,
-      height: 150, // This strictly forces the layout box height
-         
-        child: ClipRRect(
-          borderRadius: applyImageRadius ? BorderRadius.circular(borderRadius) :BorderRadius.zero,
-          child:  Image(fit:fit, image: isNetworkImage ? NetworkImage(imageUrl) : AssetImage(imageUrl) as ImageProvider,
-           // fit: BoxFit.contain,     /// 3. This forces the image to crop/scale into the box
-           //AssetImage(TImages.promoBanner2)
-            ),
-          ),
-      
-        ),
     );
   }
 }
