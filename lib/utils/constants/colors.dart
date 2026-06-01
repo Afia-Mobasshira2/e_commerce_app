8c0b3a1c91aa1b90e8672e4efe4a659d217c6321
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-01 17:44:17 +0600

    new update

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -19,9 +19,9 @@ class TColors{
     ]
   );
   //text colors
-  static const Color textprimary = Color(0xFF333333);
-  static const Color textsecondary = Color(0xFF6C7570);
-  static const Color textaccent = Colors.white;
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF6C7570);
+  static const Color textAccent = Colors.white;
 
   //background color
   static const Color light = Color(0xFFF6F6F6);
@@ -33,13 +33,13 @@ class TColors{
   static  Color darkContainer =  Colors.white.withOpacity(0.1);
 
   //button colors
-  static const Color buttonprimary = Color(0xFF4b68ff);
-  static const Color buttonsecondary = Color(0xFF6C757D);
+  static const Color buttonPrimary = Color(0xFF4b68ff);
+  static const Color buttonSecondary = Color(0xFF6C757D);
   static const Color buttonDisabled = Color(0xFFC4C4C4);
 
   //border colors
-  static const Color borderprimary = Color(0xFFD9D9D9);
-  static const Color bordersecondary = Color(0xFFE6E6E6);
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+  static const Color borderSecondary = Color(0xFFE6E6E6);
 
   //errors and validation colors
   static const Color error = Color(0xFFD32F2F);
