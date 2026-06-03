6163eee5d3f84bb32cce98d0ccef6d3b3e333615
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-03 19:12:44 +0600

    new update

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -6,6 +6,7 @@ import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class TProductCardVertical extends StatelessWidget {
   const TProductCardVertical({super.key});
@@ -37,11 +38,18 @@ class TProductCardVertical extends StatelessWidget {
               TRoundedImage(imageUrl: TImages.productShoe,applyImageRadius: true,),
 
               //Sale tag
-              TRoundedContainer(
-                radius: TSizes.sm,
-                backgroundColor: TColors.secondary.withOpacity(0.8),
-                padding: const EdgeInsets.all(value),
-              )
+              Positioned( 
+                top: 12,
+                child: TRoundedContainer(
+                  radius: TSizes.sm,
+                  backgroundColor: TColors.secondary.withOpacity(0.8),
+                  padding: const EdgeInsets.symmetric(horizontal: TSizes.sm,vertical: TSizes.xs),
+                  child: Text('25%',style: Theme.of(context).textTheme.labelLarge!.apply(color: TColors.black),),
+                ),
+              ),
+
+              //Favorite Icon Button
+              TCircularIcon(dark: dark)
             ],
           ),
           )
@@ -49,6 +57,26 @@ class TProductCardVertical extends StatelessWidget {
           ///Details
         ],
       ),
+    );
+  }
+}
+
+class TCircularIcon extends StatelessWidget {
+  const TCircularIcon({
+    super.key,
+    required this.dark,
+  });
+
+  final bool dark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+      color: dark ? TColors.black.withOpacity(0.9) : TColors.white.withOpacity(0.9),
+      ),
+      child: IconButton(onPressed: (){}, icon: const Icon(Iconsax.heart5)),
     );
   }
 }
