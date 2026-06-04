717873d16d0f532d6f8f772013a36c4e66bfa174
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-04 18:19:01 +0600

    new update

--- lib/common/widgets/layouts/grid_layout.dart
+++ lib/common/widgets/layouts/grid_layout.dart
@@ -1,2 +1,35 @@
+import 'package:e_commerce_app/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+
+class TGridLayout extends StatelessWidget {
+  const TGridLayout({
+    super.key, 
+    required this.itemCount, 
+    required this.itemBuilder, 
+    this.mainAxisExtent = 288,
+  });
+
+  final int itemCount;
+  final Widget? Function(BuildContext, int) itemBuilder;
+  final double? mainAxisExtent;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: itemCount,
+      shrinkWrap: true,
+      padding: EdgeInsets.zero,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: TSizes.gridViewSpacing,
+        crossAxisSpacing: TSizes.gridViewSpacing,
+        mainAxisExtent: mainAxisExtent
+        ), 
+      itemBuilder: itemBuilder,
+      );
+  }
+}
+
 
