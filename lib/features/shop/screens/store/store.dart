6d5c0ecfe6b36967c17d94fd98baa7605754e487
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-06 20:20:21 +0600

    new update

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/container/search_container.dart';
+import 'package:e_commerce_app/common/widgets/custom_shapes/container/t_roundeed_container.dart';
 import 'package:e_commerce_app/common/widgets/products.cart/cart_menu_icon.dart';
+import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
@@ -31,12 +33,35 @@ class StoreScreen extends StatelessWidget {
               child: ListView(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
-                children: const [
+                children: [
                   ///Search Box
-                  SizedBox(height: TSizes.spaceBtwItems,),
-                  TSearchContainer(text: '',showBorder: true,),
-                  SizedBox(height: TSizes.spaceBtwSections,),
+                  const SizedBox(height: TSizes.spaceBtwItems,),
+                  const TSearchContainer(text: '',showBorder: true,showBackground: false, padding: EdgeInsets.zero,),
+                  const SizedBox(height: TSizes.spaceBtwSections,),
 
+                  //Featured Brands
+                  TSectionHeading(title: 'Featured Brands',onPressed: (){},),
+                  const SizedBox(height: TSizes.spaceBtwItems / 1.5,),
+
+                  TRoundedContainer(
+                    padding: const EdgeInsets.all(TSizes.sm),
+                    showBorder: true,
+                    backgroundColor: Colors.transparent,
+                    child: Row(
+                      children: [
+                        //Icon
+                        Container(
+                          width: 56,
+                          height: 56,
+                          padding: const EdgeInsets.all(TSizes.sm),
+                          decoration: BoxDecoration(
+                          
+                          ),
+                          ),
+                        
+                      ],
+                    ),
+                  )
                 ],
               ),
               ),
