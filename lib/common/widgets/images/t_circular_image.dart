41c46359ecbc4ca70a167325c775567f6c58d5e0
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-06 23:12:01 +0600

    new update

--- lib/common/widgets/images/t_circular_image.dart
+++ lib/common/widgets/images/t_circular_image.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
 
+class TCircularImage extends StatelessWidget {
+  const TCircularImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 56,
+      height: 56,
+      padding: const EdgeInsets.all(TSizes.sm),
+      decoration: BoxDecoration(
+      color: THelperFunctions.isDarkMode(context) ? TColors.black : TColors.white,
+      borderRadius: BorderRadius.circular(100),
+      ),
+      child: Image(
+        image: const AssetImage(TImages.clothIcon)),
+      );
+  }
+}
--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -67,23 +67,3 @@ class StoreScreen extends StatelessWidget {
   }
 }
 
-class TCircularImage extends StatelessWidget {
-  const TCircularImage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 56,
-      height: 56,
-      padding: const EdgeInsets.all(TSizes.sm),
-      decoration: BoxDecoration(
-      color: THelperFunctions.isDarkMode(context) ? TColors.black : TColors.white,
-      borderRadius: BorderRadius.circular(100),
-      ),
-      child: Image(
-        image: const AssetImage(TImages.clothIcon)),
-      );
-  }
-}
