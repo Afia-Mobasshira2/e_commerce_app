49418b2932b11cbf888447842405d0b201dc828c
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-13 08:35:42 +0600

    new update

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:e_commerce_app/common/widgets/images/t_circular_image.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
@@ -20,6 +22,11 @@ class SettingsScreen extends StatelessWidget {
                 child: Column(
                   children: [
                     TAppBar(title: Text('Account',style: Theme.of(context).textTheme.headlineMedium!.apply(color: TColors.white),),),
+
+                    //User Profile Card
+                    ListTile(
+                      leading: const TCircularImage(image: TImages.user,width: 50,height: 50,padding: 0,),
+                    ),
                     const SizedBox(height: TSizes.spaceBtwSections,)
                   ],
                 )),
