32b148020ebcb399eec340ab9ed41aea72513e3c
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-13 19:25:00 +0600

    new update

--- lib/common/widgets/list_tiles/setting_menu_tile.dart
+++ lib/common/widgets/list_tiles/setting_menu_tile.dart
@@ -4,26 +4,29 @@ import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
-class TUserProfileTile extends StatelessWidget {
-  const TUserProfileTile({
+class TSettingsMenuTile extends StatelessWidget {
+  const TSettingsMenuTile({
     super.key, 
     required this.icon, 
     required this.title, 
-    required this.subtitle, 
-    this.trailing,
+    required this.subTitle, 
+    this.trailing, 
+    this.onTap,
   });
 
   final IconData icon;
-  final String title,subtitle;
+  final String title,subTitle;
   final Widget? trailing;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Icon(icon,size: 28,color: TColors.primary,),
       title: Text(title,style: Theme.of(context).textTheme.titleMedium),
-      subtitle: Text(subtitle,style: Theme.of(context).textTheme.labelMedium),
+      subtitle: Text(subTitle,style: Theme.of(context).textTheme.labelMedium),
       trailing: trailing,
+      onTap: onTap,
     );
   }
 }
