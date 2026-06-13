348ada97a3d1d4533c1d5a5ad67a408c4a2fdc2c
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-13 10:29:40 +0600

    new update

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -25,12 +25,7 @@ class SettingsScreen extends StatelessWidget {
                     TAppBar(title: Text('Account',style: Theme.of(context).textTheme.headlineMedium!.apply(color: TColors.white),),),
 
                     //User Profile Card
-                    ListTile(
-                      leading: const TCircularImage(image: TImages.user,width: 50,height: 50,padding: 0,),
-                      title: Text('Afia Mobasshira',style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white),),
-                      subtitle: Text('afiamobasshira2@gmail.com',style: Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white),),
-                    trailing: IconButton(onPressed: () {}, icon:const Icon(Iconsax.edit,color: TColors.white ,)),
-                    ),
+                    TUserProfileTile(),
                     const SizedBox(height: TSizes.spaceBtwSections,)
                   ],
                 )),
@@ -39,6 +34,22 @@ class SettingsScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class TUserProfileTile extends StatelessWidget {
+  const TUserProfileTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const TCircularImage(image: TImages.user,width: 50,height: 50,padding: 0,),
+      title: Text('Afia Mobasshira',style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white),),
+      subtitle: Text('afiamobasshira2@gmail.com',style: Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white),),
+    trailing: IconButton(onPressed: () {}, icon:const Icon(Iconsax.edit,color: TColors.white ,)),
     );
   }
 }
