583f6eb3befa4a3907f18c31741f53e23dbb7e4c
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-14 19:45:58 +0600

    new update

--- lib/common/widgets/list_tiles/user_profile_tile.dart
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -6,8 +6,10 @@ import 'package:iconsax/iconsax.dart';
 
 class TUserProfileTile extends StatelessWidget {
   const TUserProfileTile({
-    super.key, required Future? Function() onPressed,
+    super.key, required this.onPressed, 
   });
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +17,7 @@ class TUserProfileTile extends StatelessWidget {
       leading: const TCircularImage(image: TImages.user,width: 50,height: 50,padding: 0,),
       title: Text('Afia Mobasshira',style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white),),
       subtitle: Text('afiamobasshira2@gmail.com',style: Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white),),
-      trailing: IconButton(onPressed: () {}, icon:const Icon(Iconsax.edit,color: TColors.white ,)),
+      trailing: IconButton(onPressed: onPressed, icon:const Icon(Iconsax.edit,color: TColors.white ,)),
     );
   }
 }
--- lib/features/personalization/screens/profiles/widgets/profile.dart
+++ lib/features/personalization/screens/profiles/widgets/profile.dart
@@ -1,5 +1,10 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_app/common/widgets/images/t_circular_image.dart';
+import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -9,7 +14,40 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: TAppBar(showBackArrow: true,title: Text('Profile'),),
       ///Body
-      body:  SingleChildScrollView(),
+      body: SingleChildScrollView(
+  child: Padding(
+    padding: const EdgeInsets.all(TSizes.defaultSpace),
+    child: Column(
+      children: [
+        /// Profile Picture
+        SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              const TCircularImage(image: TImages.user, width: 80, height: 80),
+              TextButton(onPressed: () {}, child: const Text('Change Profile Picture')),
+            ],
+          ),
+        ), // SizedBox
+
+        /// Details
+        const SizedBox(height: TSizes.spaceBtwItems / 2),
+        const Divider(),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        const TSectionHeading(title: 'Profile Information', showActionButton: false),
+        const SizedBox(height: TSizes.spaceBtwItems),
+
+        Row(
+          children: [
+            Expanded(child: Text('Name', style: Theme.of(context).textTheme.bodySmall, overflow: TextOverflow.ellipsis)),
+            Expanded(child: Text('Coding with T', style: Theme.of(context).textTheme.bodyMedium, overflow: TextOverflow.ellipsis)),
+            const Expanded(child: Icon(Iconsax.arrow_right_34, size: 18)),
+          ],
+        ), // Row
+      ],
+    ), // Column
+  ), // Padding
+), // SingleChildScrollView
     );
   }
 }
