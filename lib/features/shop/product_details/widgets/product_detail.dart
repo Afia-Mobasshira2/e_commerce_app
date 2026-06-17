c64394f136e8d1e34f4bbf280dab9d53c55e626c
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-17 10:59:04 +0600

    new update

--- lib/features/shop/product_details/widgets/product_detail.dart
+++ lib/features/shop/product_details/widgets/product_detail.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/common/widgets/Icons/t_circular_icon.dart';
+import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:e_commerce_app/common/widgets/images/t_rounded_image.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
@@ -5,6 +7,7 @@ import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
@@ -17,49 +20,69 @@ class ProductDetailScreen extends StatelessWidget {
         child: Column(
           children: [
             ///-----Product image Silder
-            TCurvedEdgesWidgets(
-              child: Container(
-                color: dark ? TColors.darkerGrey : TColors.light,
-                child: Stack(
-                  children: [
-                      ///Main large image
-                     SizedBox(
-                      height: 400,child: Padding(
-                        padding: EdgeInsets.all(TSizes.productImageRadius *2 ),
-                        child: Center(child: Image(image: AssetImage(TImages.productShoe)),
-                                             ),
-                      ),
-                     ),
-                     ///---Product Details
-                    Positioned(
-                      right: 0,
-                      bottom: 20,
-                      left: TSizes.defaultSpace,
-                      child: SizedBox(
-                        height: 80,
-                        child: ListView.separated(
-                           itemCount: 6 ,
-                           shrinkWrap: true,
-                           scrollDirection: Axis.horizontal,
-                           physics: const AlwaysScrollableScrollPhysics(),
-                           separatorBuilder: (_, __) => const SizedBox(width: TSizes.spaceBtwItems,),
-                           itemBuilder: (_, index) => TRoundedImage(
-                           width: 80,
-                           backgroundColor: dark ? TColors.dark : TColors.white,
-                           border: Border.all(color: TColors.primary),
-                           padding: EdgeInsets.all(TSizes.sm),
-                           imageUrl: TImages.productShoeDetails3),),
-                      ),
-                    )
-                    
-                  ],
-                ),
-              )
-            ),
+            TProductImageSlider(dark: dark),
             
           ],
         ),
       ),
+    );
+  }
+}
+
+class TProductImageSlider extends StatelessWidget {
+  const TProductImageSlider({
+    super.key,
+    required this.dark,
+  });
+
+  final bool dark;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgesWidgets(
+      child: Container(
+        color: dark ? TColors.darkerGrey : TColors.light,
+        child: Stack(
+          children: [
+              ///Main large image
+             SizedBox(
+              height: 400,child: Padding(
+                padding: EdgeInsets.all(TSizes.productImageRadius *2 ),
+                child: Center(child: Image(image: AssetImage(TImages.productShoe)),
+                                     ),
+              ),
+             ),
+             ///---Product Details
+            Positioned(
+              right: 0,
+              bottom: 20,
+              left: TSizes.defaultSpace,
+              child: SizedBox(
+                height: 80,
+                child: ListView.separated(
+                   itemCount: 6 ,
+                   shrinkWrap: true,
+                   scrollDirection: Axis.horizontal,
+                   physics: const AlwaysScrollableScrollPhysics(),
+                   separatorBuilder: (_, __) => const SizedBox(width: TSizes.spaceBtwItems,),
+                   itemBuilder: (_, index) => TRoundedImage(
+                   width: 80,
+                   backgroundColor: dark ? TColors.dark : TColors.white,
+                   border: Border.all(color: TColors.primary),
+                   padding: EdgeInsets.all(TSizes.sm),
+                   imageUrl: TImages.productShoeDetails3),),
+              ),
+            ),
+    
+    
+            TAppBar(
+              showBackArrow: true,
+              actions: [TCircularIcon(icon: Iconsax.heart5,color: Colors.red,)],
+            )
+            
+          ],
+        ),
+      )
     );
   }
 }
