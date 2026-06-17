e942d95ec208cc20cf5a19b80ef49cdb752f1dab
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-06-17 11:30:30 +0600

    new update

--- lib/features/shop/product_details/widgets/product_detail.dart
+++ lib/features/shop/product_details/widgets/product_detail.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:e_commerce_app/common/widgets/images/t_rounded_image.dart';
 import 'package:e_commerce_app/features/shop/product_details/widgets/product_detail_image_slider.dart';
+import 'package:e_commerce_app/features/shop/product_details/widgets/rating_share_widget.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
@@ -28,25 +29,7 @@ class ProductDetailScreen extends StatelessWidget {
              child: Column(
               children: [
                 ///------Rating & Share button------
-                Row(
-                  children: [
-                /// --Rating--
-                Row(
-                  children: [
-                    const Icon(Iconsax.star5,color: Colors.amber,size: 24,),
-                    const SizedBox(width: TSizes.spaceBtwItems / 2,),
-                    Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(text: '5.0',style: Theme.of(context).textTheme.bodyLarge),
-                          const TextSpan(text: '(199)'),
-                        ] )
-                    )
-                  ],
-                )
-                /// --Share button---
-                  ],
-                )
+                TRatingAndShare()
                 
                 /// ---Price,title,stack,brand--
                 /// ---Attributes---
