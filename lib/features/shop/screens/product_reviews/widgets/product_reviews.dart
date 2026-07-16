1adc6df43d24955da1183e9228c548656a302364
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-07-16 21:22:08 +0600

    new update

--- lib/features/shop/screens/product_reviews/widgets/product_reviews.dart
+++ lib/features/shop/screens/product_reviews/widgets/product_reviews.dart
@@ -5,6 +5,9 @@ import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ProductReviewsScreen extends StatelessWidget {
   const ProductReviewsScreen({super.key});
@@ -27,7 +30,13 @@ class ProductReviewsScreen extends StatelessWidget {
 
               ///Overall prduct rating
               const TOverallProductRating(),
-              RatingBarIndicator(itemB),
+              RatingBarIndicator(
+                rating: 3.5,
+                itemSize: 20,
+                unratedColor: TColors.grey,
+                itemBuilder: (_, __) => const Icon(Iconsax.star1,color: TColors.primary,),
+                
+                ),
             ],
           ),
           ),
