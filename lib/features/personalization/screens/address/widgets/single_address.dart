712332a42c782eaaeb47bed6ab9c9b3628240b47
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-07-18 11:53:56 +0600

    new update

---
+++ lib/features/personalization/screens/address/widgets/single_address.dart
@@ -0,0 +1,39 @@
+import 'package:e_commerce_app/common/widgets/custom_shapes/container/t_roundeed_container.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TSingleAddress extends StatelessWidget {
+  const TSingleAddress({super.key, required this.selectedAddress});
+
+  final bool selectedAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return TRoundedContainer(
+        width: double.infinity,
+        showBorder: true,
+        backgroundColor: selectedAddress ? TColors.primary.withOpacity(0.5) : Colors.transparent,
+        borderColor: selectedAddress 
+        ? Colors.transparent
+        : dark
+              ? TColors.darkerGrey
+              : TColors.grey,
+        margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+        child: Stack(
+          children: [
+            Icon(selectedAddress ? Iconsax.tick_circle5 : null,
+            color: selectedAddress
+                ? dark
+                      ? TColors.light
+                      : TColors.dark
+                      : null,
+            ),
+          ],
+        ),
+    );
+  }
+}
