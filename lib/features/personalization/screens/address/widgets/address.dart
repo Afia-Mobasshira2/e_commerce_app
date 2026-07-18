d9eb38bab40bddfc70ac469e046885f25231b406
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-07-18 10:55:56 +0600

    new update

--- lib/features/personalization/screens/address/widgets/address.dart
+++ lib/features/personalization/screens/address/widgets/address.dart
@@ -1,10 +1,26 @@
+import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_app/features/personalization/screens/address/widgets/add_new_address.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:iconsax/iconsax.dart';
 
 class UserAddressScreen extends StatelessWidget {
   const UserAddressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: TColors.primary,
+        onPressed: () => Get.to(() => const AddNewAddressScreen()),
+         child: const Icon(Iconsax.add,color: TColors.white,),
+        ),
+        appBar: TAppBar(
+          showBackArrow: true,
+          title: Text('Addresses',style: Theme.of(context).textTheme.headlineSmall,),
+        ),
+    );
   }
 }
