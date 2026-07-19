e2ca3ae024303807f36ceb363a33d737fa693f09
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-07-19 21:16:49 +0600

    new update

--- lib/features/shop/screens/cart/widgets/cart.dart
+++ lib/features/shop/screens/cart/widgets/cart.dart
@@ -25,45 +25,56 @@ class CartScreen extends StatelessWidget {
             separatorBuilder: (_, __) =>  const SizedBox(height: TSizes.defaultSpace,) ,
             itemBuilder: (_,index) => Column(
               children: [
-                Row(
-                  children: [
-                    //Image
-                    TRoundedImage(
-                    imageUrl: TImages.storeSportsShoe1,
-                    width: 60,
-                    height: 60,
-                    padding: EdgeInsets.all(TSizes.sm),
-                    backgroundColor: THelperFunctions.isDarkMode(context) ? TColors.darkerGrey : TColors.light,
-                    ),
-                    const SizedBox(width: TSizes.spaceBtwItems,),
-
-                    //Title,Price ,Size
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                          const TBrandTitleTextWithVerifiedIcon(title: 'Nike'),
-                          const TProductTitleText(title: 'Black Sports shoe', maxLines: 1,),
-
-                      //Attributes
-                      Text.rich(
-                        TextSpan(
-                          children: [
-                          TextSpan(text: "Color",style: Theme.of(context).textTheme.bodySmall),
-                          TextSpan(text: "Green",style: Theme.of(context).textTheme.bodyLarge),
-                          TextSpan(text: "Size",style: Theme.of(context).textTheme.bodySmall),
-                          TextSpan(text: "WR 10",style: Theme.of(context).textTheme.bodyLarge),
-                          ]
-                          )
-                      )
-                      ],
-                    )
-                  ],
-                )
+                TCartItem()
               ],
             ),
           ),
           ),
         ),
+    );
+  }
+}
+
+class TCartItem extends StatelessWidget {
+  const TCartItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        //Image
+        TRoundedImage(
+        imageUrl: TImages.storeSportsShoe1,
+        width: 60,
+        height: 60,
+        padding: EdgeInsets.all(TSizes.sm),
+        backgroundColor: THelperFunctions.isDarkMode(context) ? TColors.darkerGrey : TColors.light,
+        ),
+        const SizedBox(width: TSizes.spaceBtwItems,),
+    
+        //Title,Price ,Size
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+              const TBrandTitleTextWithVerifiedIcon(title: 'Nike'),
+              const TProductTitleText(title: 'Black Sports shoe', maxLines: 1,),
+    
+          //Attributes
+          Text.rich(
+            TextSpan(
+              children: [
+              TextSpan(text: "Color",style: Theme.of(context).textTheme.bodySmall),
+              TextSpan(text: "Green",style: Theme.of(context).textTheme.bodyLarge),
+              TextSpan(text: "Size",style: Theme.of(context).textTheme.bodySmall),
+              TextSpan(text: "WR 10",style: Theme.of(context).textTheme.bodyLarge),
+              ]
+              )
+          )
+          ],
+        )
+      ],
     );
   }
 }
