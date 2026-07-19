ee4f36eed146151484a95b7ba6a373f2acdba312
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-07-19 23:12:55 +0600

    new update

--- lib/features/shop/screens/cart/widgets/cart.dart
+++ lib/features/shop/screens/cart/widgets/cart.dart
@@ -48,37 +48,3 @@ class CartScreen extends StatelessWidget {
   }
 }
 
-class TProductQuantityWithAddRemoveButton extends StatelessWidget {
-  const TProductQuantityWithAddRemoveButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        TCircularIcon(icon: Iconsax.minus,
-        width: 32,
-        height: 32,
-        size: TSizes.md,
-        color: THelperFunctions.isDarkMode(context) ? TColors.white : TColors.black,
-        backgroundColor: THelperFunctions.isDarkMode(context) ? TColors.darkerGrey : TColors.light,
-        ),
-        const SizedBox(width: TSizes.spaceBtwItems),
-          Text('2',style: Theme.of(context).textTheme.titleSmall,),
-        const SizedBox(width: TSizes.spaceBtwItems),
-    
-        TCircularIcon(
-          icon: Iconsax.add,
-          width: 32,
-          height: 32,
-          size: TSizes.md,
-          color: TColors.white,
-          backgroundColor: TColors.primary,
-        ),
-      ],
-    );
-  }
-}
-
