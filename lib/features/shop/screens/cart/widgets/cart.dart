0004c80682bd93e5e377c96ae1486d9c299fce47
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-07-19 09:47:21 +0600

    new update

---
+++ lib/features/shop/screens/cart/widgets/cart.dart
@@ -0,0 +1,46 @@
+import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_app/common/widgets/images/t_rounded_image.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        showBackArrow: true,title: Text('Cart',style: Theme.of(context).textTheme.headlineSmall),),
+        body: SingleChildScrollView(
+          child: Padding(padding: EdgeInsets.all(TSizes.defaultSpace),
+          child:
+          ListView.separated(
+            shrinkWrap: true,
+            itemCount: 4,
+            separatorBuilder: (_, __) =>  const SizedBox(height: TSizes.defaultSpace,) ,
+            itemBuilder: (_,index) => Column(
+              children: [
+                Row(
+                  children: [
+                    //Image
+                    TRoundedImage(
+                    imageUrl: TImages.storeSportsShoe1,
+                    width: 60,
+                    height: 60,
+                    padding: EdgeInsets.all(TSizes.sm),
+                    backgroundColor: THelperFunctions.isDarkMode(context) ? TColors.darkerGrey : TColors.light,
+                    ),
+                    const SizedBox(width: TSizes.defaultSpace,)
+                  ],
+                )
+              ],
+            ),
+          ),
+          ),
+        ),
+    );
+  }
+}
